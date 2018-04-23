59b5fc1a5d4e4dab78f766560d623e9eabee0a4c
Tim Fabritius <tc.fabritius@gmail.com>
2018-04-23 19:11:00 +0300

    Translated

--- admin/home.php
+++ admin/home.php
@@ -1,17 +1,17 @@
-﻿<?php  
-session_start();  
+﻿<?php
+session_start();
 if(!isset($_SESSION["user"]))
 {
  header("location:index.php");
 }
-?> 
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Administrator	</title>
+    <title>Järjestelmänvalvoja	</title>
     <!-- Bootstrap Styles-->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FontAwesome Styles-->
@@ -43,12 +43,12 @@ if(!isset($_SESSION["user"]))
                         <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="usersetting.php"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        <li><a href="usersetting.php"><i class="fa fa-user fa-fw"></i> Käyttäjäprofiili</a>
                         </li>
-                        <li><a href="settings.php"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        <li><a href="settings.php"><i class="fa fa-gear fa-fw"></i> Asetukset</a>
                         </li>
                         <li class="divider"></li>
-                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Kirjaudu ulos</a>
                         </li>
                     </ul>
                     <!-- /.dropdown-user -->
@@ -62,27 +62,27 @@ if(!isset($_SESSION["user"]))
                 <ul class="nav" id="main-menu">
 
                     <li>
-                        <a class="active-menu" href="home.php"><i class="fa fa-dashboard"></i> Status</a>
+                        <a class="active-menu" href="home.php"><i class="fa fa-dashboard"></i> Tila</a>
                     </li>
                     <li>
-                        <a href="messages.php"><i class="fa fa-desktop"></i> News Letters</a>
+                        <a href="messages.php"><i class="fa fa-desktop"></i> Uutiskirjeet</a>
                     </li>
 					<li>
-                        <a href="roombook.php"><i class="fa fa-bar-chart-o"></i> Room Booking</a>
+                        <a href="roombook.php"><i class="fa fa-bar-chart-o"></i> Huone varaukset</a>
                     </li>
                     <li>
-                        <a href="payment.php"><i class="fa fa-qrcode"></i> Payment</a>
+                        <a href="payment.php"><i class="fa fa-qrcode"></i> Maksut</a>
                     </li>
                     <li>
-                        <a  href="profit.php"><i class="fa fa-qrcode"></i> Profit</a>
+                        <a  href="profit.php"><i class="fa fa-qrcode"></i> Voitot</a>
                     </li>
                     <li>
-                        <a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        <a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Kirjaudu ulos</a>
                     </li>
-                   
 
 
-                    
+
+
 					</ul>
 
             </div>
@@ -96,7 +96,7 @@ if(!isset($_SESSION["user"]))
                 <div class="row">
                     <div class="col-md-12">
                         <h1 class="page-header">
-                            Status <small>Room Booking </small>
+                            Tila <small>Huone varaus </small>
                         </h1>
                     </div>
                 </div>
@@ -114,33 +114,33 @@ if(!isset($_SESSION["user"]))
 								if($new=="Not Conform")
 								{
 									$c = $c + 1;
-									
-								
-								}
-						
-						}
-						
-									
-									
 
-						
+
+								}
+
+						}
+
+
+
+
+
 				?>
 
 					<div class="row">
                 <div class="col-md-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            
+
                         </div>
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
-							
+
 							<div class="panel panel-primary">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
                                             <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
 											<button class="btn btn-default" type="button">
-												 New Room Bookings  <span class="badge"><?php echo $c ; ?></span>
+												 Uudet huonevaraukset  <span class="badge"><?php echo $c ; ?></span>
 											</button>
 											</a>
                                         </h4>
@@ -148,34 +148,34 @@ if(!isset($_SESSION["user"]))
                                     <div id="collapseTwo" class="panel-collapse in" style="height: auto;">
                                         <div class="panel-body">
                                            <div class="panel panel-default">
-                        
+
                         <div class="panel-body">
                             <div class="table-responsive">
                                 <table class="table">
                                     <thead>
                                         <tr>
                                             <th>#</th>
-                                            <th>Name</th>
-                                            <th>Email</th>
-                                            <th>Country</th>
-											<th>Room</th>
-											<th>Bedding</th>
-											<th>Meal</th>
-											<th>Check In</th>
-											<th>Check Out</th>
-											<th>Status</th>
-											<th>More</th>
-											
+                                            <th>Nimi</th>
+                                            <th>Sähköposti</th>
+                                            <th>Valtio</th>
+											<th>Huone</th>
+											<th>Vuodevaatteet</th>
+											<th>Ateria</th>
+											<th>Sisäänkirjautuminen</th>
+											<th>Uloskirjautuminen</th>
+											<th>Tila</th>
+											<th>Lisää</th>
+
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        
+
 									<?php
 									$tsql = "select * from roombook";
 									$tre = mysqli_query($con,$tsql);
 									while($trow=mysqli_fetch_array($tre) )
-									{	
-										$co =$trow['stat']; 
+									{
+										$co =$trow['stat'];
 										if($co=="Not Conform")
 										{
 											echo"<tr>
@@ -189,52 +189,52 @@ if(!isset($_SESSION["user"]))
 												<th>".$trow['cin']."</th>
 												<th>".$trow['cout']."</th>
 												<th>".$trow['stat']."</th>
-												
+
 												<th><a href='roombook.php?rid=".$trow['id']." ' class='btn btn-primary'>Action</a></th>
 												</tr>";
-										}	
-									
+										}
+
 									}
 									?>
-                                        
+
                                     </tbody>
                                 </table>
-								
+
                             </div>
                         </div>
                     </div>
-                      <!-- End  Basic Table  --> 
+                      <!-- End  Basic Table  -->
                                         </div>
                                     </div>
                                 </div>
 								<?php
-								
+
 								$rsql = "SELECT * FROM `roombook`";
 								$rre = mysqli_query($con,$rsql);
 								$r =0;
 								while($row=mysqli_fetch_array($rre) )
-								{		
+								{
 										$br = $row['stat'];
 										if($br=="Conform")
 										{
 											$r = $r + 1;
-											
-											
-											
+
+
+
 										}
-										
-								
+
+
 								}
-						
+
 								?>
                                 <div class="panel panel-info">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
                                             <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="collapsed">
 											<button class="btn btn-primary" type="button">
-												 Booked Rooms  <span class="badge"><?php echo $r ; ?></span>
+												 Varatut huoneet  <span class="badge"><?php echo $r ; ?></span>
 											</button>
-											
+
 											</a>
                                         </h4>
                                     </div>
@@ -243,14 +243,14 @@ if(!isset($_SESSION["user"]))
 										<?php
 										$msql = "SELECT * FROM `roombook`";
 										$mre = mysqli_query($con,$msql);
-										
+
 										while($mrow=mysqli_fetch_array($mre) )
-										{		
+										{
 											$br = $mrow['stat'];
 											if($br=="Conform")
 											{
 												$fid = $mrow['id'];
-												 
+
 											echo"<div class='col-md-3 col-sm-12 col-xs-12'>
 													<div class='panel panel-primary text-center no-boder bg-color-blue'>
 														<div class='panel-body'>
@@ -263,42 +263,42 @@ if(!isset($_SESSION["user"]))
 													</button></a>
 															".$mrow['TRoom']."
 														</div>
-													</div>	
+													</div>
 											</div>";
-															
-												
-					
-				
-												
+
+
+
+
+
 											}
-											
-									
+
+
 										}
 										?>
-                                           
+
 										</div>
-										
+
                                     </div>
-									
+
                                 </div>
                                 <?php
-								
+
 								$fsql = "SELECT * FROM `contact`";
 								$fre = mysqli_query($con,$fsql);
 								$f =0;
 								while($row=mysqli_fetch_array($fre) )
 								{
 										$f = $f + 1;
-								
+
 								}
-						
+
 								?>
                                 <div class="panel panel-danger">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
                                             <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="collapsed">
 											<button class="btn btn-primary" type="button">
-												 Followers  <span class="badge"><?php echo $f ; ?></span>
+												 Seuraajat  <span class="badge"><?php echo $f ; ?></span>
 											</button>
 											</a>
                                         </h4>
@@ -311,22 +311,22 @@ if(!isset($_SESSION["user"]))
                                     <thead>
                                         <tr>
                                             <th>#</th>
-                                            <th>Full Name</th>
-                                            <th>Email</th>
-											<th>Follow Start</th>
-                                            <th>Permission status</th>
-                                            
-											
+                                            <th>Koko nimi</th>
+                                            <th>Sähköposti</th>
+											<th>Alkoi seurata</th>
+                                            <th>Valtuustila</th>
+
+
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        
+
 									<?php
 									$csql = "select * from contact";
 									$cre = mysqli_query($con,$csql);
 									while($crow=mysqli_fetch_array($cre) )
-									{	
-										
+									{
+
 											echo"<tr>
 												<th>".$crow['id']."</th>
 												<th>".$crow['fullname']."</th>
@@ -334,11 +334,11 @@ if(!isset($_SESSION["user"]))
 												<th>".$crow['cdate']." </th>
 												<th>".$crow['approval']."</th>
 												</tr>";
-										
-									
+
+
 									}
 									?>
-                                        
+
                                     </tbody>
                                 </table>
 								<a href="messages.php" class="btn btn-primary">More Action</a>
@@ -353,19 +353,19 @@ if(!isset($_SESSION["user"]))
                     </div>
                 </div>
             </div>
-            
-			
+
+
 				<!-- DEOMO-->
 				<div class='panel-body'>
                             <button class='btn btn-primary btn' data-toggle='modal' data-target='#myModal'>
-                              Update 
+                              Päivitä
                             </button>
                             <div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
                                 <div class='modal-dialog'>
                                     <div class='modal-content'>
                                         <div class='modal-header'>
                                             <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-                                            <h4 class='modal-title' id='myModalLabel'>Change the User name and Password</h4>
+                                            <h4 class='modal-title' id='myModalLabel'>Vaihda käyttäjänimi ja salasana</h4>
                                         </div>
 										<form method='post>
                                         <div class='modal-body'>
@@ -380,26 +380,26 @@ if(!isset($_SESSION["user"]))
                                             <input name='pasd' value='<?php echo $ps; ?>' class='form-control' placeholder='Enter Password'>
 											</div>
                                         </div>
-										
+
                                         <div class='modal-footer'>
                                             <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
-											
+
                                            <input type='submit' name='up' value='Update' class='btn btn-primary'>
 										  </form>
-										   
+
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-				
+
 				<!--DEMO END-->
-				
-										
-                    
+
+
+
 
                 <!-- /. ROW  -->
-				
+
             </div>
             <!-- /. PAGE INNER  -->
         </div>
