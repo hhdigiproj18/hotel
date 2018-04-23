16c80a5f1fdd91bad953ec757e3b0a44142060f1
jpirttimaa <38536940+jpirttimaa@users.noreply.github.com>
2018-04-23 20:13:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -145,7 +145,7 @@ include('db.php');
 									<div class="modal-header">
 										<button type="button" class="close" data-dismiss="modal">&times;</button>
 										<h4>SUN  <span>RISE</span></h4>
-										<img src="images/1.jpg" alt=" " class="img-responsive">
+										<img src="images/Uudet kuvat/etusivu1.jpg" alt=" " class="img-responsive">
 										<h5>We know what you love</h5>
 										<p>Providing guests unique and enchanting views from their rooms with its exceptional amenities, makes Star Hotel one of bests in its kind.Try our food menu, awesome services and friendly staff while you are here.</p>
 									</div>
