0244e2566cdd161b6df203aa8b98185a9a4512f3
Tim Fabritius <tc.fabritius@gmail.com>
2018-05-11 13:14:57 +0300

    Team section added

--- index.php
+++ index.php
@@ -180,7 +180,103 @@ include('db.php');
     </div>
 </div>
  	<!-- //about -->
-
+<!-- team -->
+<div class="team" id="team">
+	<div class="container">
+			<h3 class="title-w3-agileits title-black-wthree">Meet Our Team</h3>
+			<div id="horizontalTab">
+					<ul class="resp-tabs-list">
+					<li>
+						<img src="images/teams1.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/teams2.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/teams3.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/teams4.jpg" alt=" " class="img-responsive" />
+					</li>
+					</ul>
+					<div class="resp-tabs-container">
+					<div class="tab1">
+						<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Lucas Jimenez</h4>
+							<span>Manager</span>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.Lorem ipsum dolor .</p>
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab2">
+					<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Sarah Connor</h4>
+							<span>Receptionist</span>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.Lorem ipsum dolor .</p>	
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab3">
+						<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Ivan Simpson</h4>
+							<span>Manager</span>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.Lorem ipsum dolor .</p>
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab4">
+					<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Marc Gutierrez</h4>
+							<span>Receptionist</span>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.Lorem ipsum dolor .</p>
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					</div>
+			</div>
+	</div>
+</div>
+<!-- //team -->
 
 <!-- Gallery -->
 <section class="portfolio-w3ls" id="gallery">
