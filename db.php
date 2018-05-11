f0aed49da2d71d2b3a0dcde7bfc7a41d306cb0ae
Tim Fabritius <tc.fabritius@gmail.com>
2018-05-11 15:33:59 +0300

    hostinger

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","","hotel") or die(mysql_error());
+$con = mysqli_connect("localhost","u571325269_ha","development18","u571325269_hrs") or die(mysql_error());
 
 ?>
